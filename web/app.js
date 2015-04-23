ee71555495cce8b229f52c14e46e924c7e0c7115
flatbird <flatbird.hiratori@gmail.com>
2015-04-23 16:35:47 +0900

    implement the web app.

--- web/app.js
+++ web/app.js
@@ -1,16 +1,88 @@
 $(function () {
-	$('#toggle').click(function () {
-		$.get('/properties/get', function (data) {
-			data.forEach(function(item) {
-				if (item.value === 'on') {
-					item.value = 'off';
-				} else {
-					item.value = 'on';
-				}
-			});
-			$.post('/properties/set', data, function (data) {
-				$('#output').text(JSON.stringify(data, undefined, '  '));
-			});
-		});
+	var swGreen;
+	var swYellow;
+	var swRed;
+	var blink = $('#blink');
+	var properties;
+	var isHidden = false;
+
+	$.get('/properties/get', function (data, status) {
+		console.log('/properties/get', status);
+		if (data) {
+			properties = data;
+			init();
+		}
 	});
+
+	function init() {
+		swGreen = new SwitchLightView('#green', 'green', 'green-off');
+		swYellow = new SwitchLightView('#yellow', 'yellow', 'yellow-off');
+		swRed = new SwitchLightView('#red', 'red', 'red-off');
+		swGreen.change(onChange);
+		swYellow.change(onChange);
+		swRed.change(onChange);
+		blink.change(onBlinkChange);
+		document.addEventListener('visibilitychange', onVisibilityChange, false);
+	}
+
+	function onBlinkChange() {
+		var isBlink = blink.get(0).checked;
+		swGreen.setBlink(isBlink);
+		swYellow.setBlink(isBlink);
+		swRed.setBlink(isBlink);
+
+		var on = isBlink ? 'blink' : 'on'; 
+		properties.forEach(function (item) {
+			var swObj;
+			var name = item.name;
+			if (name === 'green') {
+				swObj = swGreen;
+			} else if (name === 'yellow') {
+				swObj = swYellow;
+			} else if (name === 'red') {
+				swObj = swRed;
+			}
+			item.value = swObj.enabled ? on : 'off';
+		});
+		$.post('/properties/set', properties, function (data) {
+			properties = data;
+		});
+	};
+
+	function onChange(swObj) {
+		if (isHidden) return;
+		properties.forEach(function (item) {
+			var name = item.name;
+			if (name === swObj.onClass) {
+				if (swObj.enabled) {
+					item.value = swObj.blink ? 'blink' : 'on';
+				} else {
+					item.value = 'off';
+				}
+			}
+		});
+		$.post('/properties/set', properties, function (data) {
+			properties = data;
+		});
+	}
+
+	function turnOffAll() {
+		properties.forEach(function (item) {
+			item.value = 'off';
+		});
+		$.post('/properties/set', properties);
+		blink.get(0).checked = false;
+		swGreen.reset();
+		swYellow.reset();
+		swRed.reset();		
+	}
+
+	function onVisibilityChange() {
+    if (document.hidden) {
+			isHidden = true;
+    	turnOffAll();
+    } else  {
+			isHidden = false;
+    }
+	}
 });
---
+++ web/switch-light-view.js
@@ -0,0 +1,74 @@
+var BLINK_INTERVAL = 1000;
+
+// use jquery
+var SwitchLightView = function(elm, onClass, offClass) {
+	this.el = $(elm);
+	this.onClass = onClass;
+	this.offClass = offClass;
+	this.enabled = false;
+	this.onChange = null;
+	this.blink = false;
+	this.el.addClass(this.offClass);
+	this.el.click(this._onClick.bind(this));
+}
+
+SwitchLightView.prototype._onClick = function () {
+	this._setLight(!this.enabled);
+}
+
+SwitchLightView.prototype._setLight = function (enabled) {
+	this.enabled = enabled;
+	var toAdd = this.enabled ? this.onClass : this.offClass;
+	var toRemove = this.enabled ? this.offClass : this.onClass;
+	this.el.addClass(toAdd);
+	this.el.removeClass(toRemove);
+	this._handleBlink();
+	if (this.onChange) {
+		this.onChange(this);
+	}
+}
+
+SwitchLightView.prototype._startBlink = function () {
+	if (!this.intervalID) {
+		this.intervalID = setInterval(toggleLight.bind(this), BLINK_INTERVAL);
+	}
+	function toggleLight() {
+		this.el.toggleClass(this.onClass);
+		this.el.toggleClass(this.offClass);
+	}
+}
+
+SwitchLightView.prototype._stopBlink = function () {
+	if (this.intervalID) {
+		clearInterval(this.intervalID);
+		this.intervalID = null;
+		this._setLight(this.enabled);
+	}
+}
+
+SwitchLightView.prototype._handleBlink = function () {
+	if (this.enabled && this.blink) {
+		this._startBlink();
+	} else {
+		this._stopBlink();
+	}
+}
+
+//
+// Public methods
+//
+SwitchLightView.prototype.change = function(callback) {
+	this.onChange = callback;
+}
+
+SwitchLightView.prototype.setBlink = function(blink) {
+	this.blink = blink;
+	this._handleBlink();
+}
+
+SwitchLightView.prototype.reset = function() {
+	this.blink = false;
+	this._stopBlink();
+	this._setLight(false);
+}
+
